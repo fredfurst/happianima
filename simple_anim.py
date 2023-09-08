cf325b035ad2a35ec4b39505e5ab715e0a8d9686
fredfurst <ffbittencourt@gmail.com>
2023-09-08 11:49:16 -0300

    added the simplest animation in matplotlib

---
+++ simple_anim.py
@@ -0,0 +1,30 @@
+##
+## Matplotlib Simpler Example
+##
+
+# Simplified from 
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+
+t = np.linspace(0, 3, 40)
+g = -9.81
+v02 = 5
+z2 = g * t**2 / 2 + v02 * t
+
+fig, ax = plt.subplots()
+
+line2 = ax.plot(t[0], z2[0], label=f'v0 = {v02} m/s')[0]
+ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
+ax.legend()
+
+def update(frame):
+    # update the line plot:
+    line2.set_xdata(t[:frame])
+    line2.set_ydata(z2[:frame])
+    return line2
+
+
+ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=30)
+plt.show()
