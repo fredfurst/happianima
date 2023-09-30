7deac2fe76f37ca7c8641f85f1feb386d496c418
fredfurst <ffbittencourt@gmail.com>
2023-09-30 18:36:57 -0300

    new files, new toys

--- simple_anim.py
+++ simple_anim.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-t = np.linspace(0, 3, 40)
+t = np.linspace(0, 3, 400)
 g = -9.81
 v02 = 5
 z2 = g * t**2 / 2 + v02 * t
@@ -16,7 +16,7 @@ z2 = g * t**2 / 2 + v02 * t
 fig, ax = plt.subplots()
 
 line2 = ax.plot(t[0], z2[0], label=f'v0 = {v02} m/s')[0]
-ax.set(xlim=[0, 3], ylim=[-4, 10], xlabel='Time [s]', ylabel='Z [m]')
+ax.set(xlim=[-1, 4], ylim=[-30, 3], xlabel='Time [s]', ylabel='Z [m]')
 ax.legend()
 
 def update(frame):
@@ -26,5 +26,6 @@ def update(frame):
     return line2
 
 
-ani = animation.FuncAnimation(fig=fig, func=update, frames=40, interval=30)
+ani = animation.FuncAnimation(fig=fig, func=update, frames=400, interval=30)
 plt.show()
+ani.save('animation.mp4')
